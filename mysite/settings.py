328ec0da9651ba4724640f900612b43dc1209b84
rinat.idiiatov <rinat.idiiatov@qlever.com>
2022-07-24 14:16:25 +0300

    настройки для публикации в хероку

--- mysite/settings.py
+++ mysite/settings.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -14,7 +16,7 @@ SECRET_KEY = 'django-insecure-p(nryb4@o%3u&&#uxo2utm90ftbrm*t$!&3pe1m!)lo3%p&efq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chat-pet.herokuapp.com']
 
 
 # Application definition
@@ -109,7 +111,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
